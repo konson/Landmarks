f2ee74b45bf97ea6bfd907af9f9686fa8105b26c
Alecs Konson <alecs429@gmail.com>
2024-01-25 09:21:02 -0500

    creating and combining views

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
-@main
-struct LandmarksApp: App {
-    var body: some Scene {
-        WindowGroup {
+@main // entry into app
+
+struct LandmarksApp: App { // conforms to App protocol
+    var body: some Scene { // returns one or more scenes for display
+        
+//      WindowGroup: used for defining the root structure of an app and managing
+//      the hierarchy of windows in a multi-window environment, such as on iPadOS
+//      and macOS.
+        WindowGroup { // Think of it as the top-level container that holds the main UI components of your app.
             ContentView()
         }
     }
