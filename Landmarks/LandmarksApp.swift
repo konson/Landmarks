fc7fbf8c25b031ea105aa4f3bb81a127dd8dd413
Alecs Konson <alecs429@gmail.com>
2024-01-25 05:22:02 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alecs Konson on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
